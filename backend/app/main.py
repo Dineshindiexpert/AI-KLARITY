a13a028f4faf63d38eaec2366111747b0cde7373
dineshindixpert <dineshjangra0062@gmail.com>
2026-06-27 18:28:11 +0530

    hanlde error

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ai-klarity-4oia.vercel.app/"
+        "https://ai-klarity-4oia.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
